4dcc7283a4631e839a3a875c0abcbe2393499e3a
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-01-26 19:47:36 -0800

    Finished project and ready to submit!

---
+++ main.js
@@ -0,0 +1,39 @@
+
+//Set myAge to my age!
+const myAge = 28;
+
+
+//Set earlyYears to 2
+let earlyYears = 2;
+
+
+// Use the multiplication assignment operator to multiply the value saved to earlyYears by 10.5 and reassign it to earlyYears.
+earlyYears *= 10.5;
+
+
+// Take the myAge variable, and subtract 2 from it. Assign to laterYears
+let laterYears = myAge - 2
+
+
+// Multiply the laterYears variable by 4 
+laterYears *= 4;
+
+
+// If you’d like to check your work at this point, console log earlyYears and laterYears. Are the values what you expected?
+// console.log(earlyYears);
+// console.log(laterYears);
+
+
+// Add earlyYears and laterYears together
+let myAgeInDogYears = earlyYears + laterYears;
+
+
+//Take my name and set it to lowercase
+const myName = "Carly".toLowerCase();
+
+
+// Replace NAME with myName, HUMAN AGE with myAge, and DOG AGE with myAgeInDogYears in the sentence above.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+// If you’d like extra practice, try writing this project without the *= operator.
